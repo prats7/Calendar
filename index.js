69af8bb3943fadd2224a043615b8a3b21db0f192
Pratyush Vishal <pratyushvishal04@gmail.com>
2023-01-09 01:51:40 +0530

    display month name

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function load() {
     });
     const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
 
+    document.getElementById('monthDisplay').innerText = `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+
     for (let i = 1; i <= paddingDays + daysInMonth; i++) {
 
         const daySquare = document.createElement('div');
